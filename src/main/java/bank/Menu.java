b7be451e6350f315c6c416eeffccea955393bc0e
hakiminazli <67014568+hakiminazli@users.noreply.github.com>
2024-06-07 08:37:47 +0000

    Create Authenticator

---
+++ src/main/java/bank/Menu.java
@@ -0,0 +1,18 @@
+package bank;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner scanner;
+    public static void main(String[] args) {
+        System.out.println("Welcome to RHB Bank");
+
+        Menu menu = new Menu();
+        menu.scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+
+
+}
